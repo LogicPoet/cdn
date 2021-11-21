efd05cd7378be38c5d5c1ec5824965ceebf6895f
xiyan <48608438+huaxiexiyan@users.noreply.github.com>
2021-11-21 14:36:58 +0800

    Update start-openresty-docker.sh
    修改启动配置

--- scripts/start-openresty-docker.sh
+++ scripts/start-openresty-docker.sh
@@ -11,7 +11,8 @@ services:
     nginx:
         # 容器名称
         container_name: "openresty"
-        image: openresty/openresty:1.19.9.1-3-alpine-fat
+        # image: openresty/openresty:1.19.9.1-3-alpine-fat
+        image: openresty/openresty:1.19.9.1-3-centos7-amd64
         # 端口映射
         ports:
             - "80:80"
@@ -23,8 +24,11 @@ services:
             # 映射主机./conf.d目录到容器/etc/nginx/conf.d目录
             - "/root/applications/openresty/conf/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf"
             - "/root/applications/openresty/conf/conf.d/:/etc/nginx/conf.d/"
-            - "/root/applications/openresty/logs/:/var/log/nginx/"
+            - "/root/applications/openresty/logs/:/usr/local/openresty/nginx/logs"
             - "/root/applications/openresty/socket/:/usr/share/socket"
+            - "/var/run/docker.sock:/var/run/docker.sock" 
+            - "/usr/bin/docker:/usr/bin/docker"
+            - "/usr/lib64/libltdl.so.7:/usr/lib/x86_64-linux-gnu/libltdl.so.7"
 EOF
 # 创建配置文件
 tee /root/applications/openresty/conf/nginx.conf <<-'EOF'
@@ -48,9 +52,10 @@ tee /root/applications/openresty/conf/nginx.conf <<-'EOF'
 
 #user  nobody;
 #worker_processes 1;
+worker_processes auto;
 
 # Enables the use of JIT for regular expressions to speed-up their processing.
-pcre_jit on;
+# pcre_jit on;
 
 
 
@@ -58,10 +63,16 @@ pcre_jit on;
 #error_log  logs/error.log  notice;
 #error_log  logs/error.log  info;
 
-#pid        logs/nginx.pid;
+error_log  logs/error.log  error;
+error_log  logs/notice.log  notice;
+error_log  logs/info.log  info;
+
+
+pid        logs/nginx.pid;
 
 
 events {
+    use                 epoll;
     worker_connections  1024;
 }
 
@@ -74,28 +85,29 @@ http {
     # When the use of underscores is disabled, request header fields whose names contain underscores are marked as invalid and become subject to the ignore_invalid_headers directive.
     # underscores_in_headers off;
 
-    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-    #                  '$status $body_bytes_sent "$http_referer" '
-    #                  '"$http_user_agent" "$http_x_forwarded_for"';
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
 
-    #access_log  logs/access.log  main;
+    access_log  logs/access.log  main;
 
-        # Log in JSON Format
-        # log_format nginxlog_json escape=json '{ "timestamp": "$time_iso8601", '
-        # '"remote_addr": "$remote_addr", '
-        #  '"body_bytes_sent": $body_bytes_sent, '
-        #  '"request_time": $request_time, '
-        #  '"response_status": $status, '
-        #  '"request": "$request", '
-        #  '"request_method": "$request_method", '
-        #  '"host": "$host",'
-        #  '"upstream_addr": "$upstream_addr",'
-        #  '"http_x_forwarded_for": "$http_x_forwarded_for",'
-        #  '"http_referrer": "$http_referer", '
-        #  '"http_user_agent": "$http_user_agent", '
-        #  '"http_version": "$server_protocol", '
-        #  '"nginx_access": true }';
-        # access_log /dev/stdout nginxlog_json;
+    #Log in JSON Format
+    log_format nginxlog_json escape=json '{ "timestamp": "$time_iso8601", '
+         '"remote_addr": "$remote_addr", '
+          '"body_bytes_sent": $body_bytes_sent, '
+          '"request_time": $request_time, '
+          '"response_status": $status, '
+          '"request": "$request", '
+          '"request_method": "$request_method", '
+          '"host": "$host",'
+          '"upstream_addr": "$upstream_addr",'
+          '"http_x_forwarded_for": "$http_x_forwarded_for",'
+          '"http_referrer": "$http_referer", '
+          '"http_user_agent": "$http_user_agent", '
+          '"http_version": "$server_protocol", '
+          '"nginx_access": true }';
+    #access_log /dev/stdout nginxlog_json;
+    access_log logs/stdout.log nginxlog_json;
 
     # See Move default writable paths to a dedicated directory (#119)
     # https://github.com/openresty/docker-openresty/issues/119
@@ -111,12 +123,37 @@ http {
     #keepalive_timeout  0;
     keepalive_timeout  65;
 
-    #gzip  on;
+    gzip  on;
 
     include /etc/nginx/conf.d/*.conf;
 
     # Don't reveal OpenResty version to clients.
     # server_tokens off;
+}
+EOF
+# 添加ci-cd配置
+tee /root/applications/openresty/conf/conf.d/main.conf <<-'EOF'
+server {
+    listen 80;
+    # 接收webhook完成部署的最后步骤
+    location /ci-cd {
+        content_by_lua_block {
+            ngx.req.read_body()
+            local data = ngx.req.get_body_data()
+            ngx.log(ngx.INFO, "body:", data)
+            local json = require("cjson.safe")
+            local body = json.decode(data)
+            if body then
+                local repo_name_tag = body["repository"]["repo_name"]..":"..body["push_data"]["tag"]
+                ngx.log(ngx.INFO, "repo_name_tag:", repo_name_tag)
+                local ret1 = os.execute("docker pull "..repo_name_tag)
+                ngx.log(ngx.INFO, "pull_result:", ret)
+                os.execute("docker stop "..repo_name_tag)
+                os.execute("docker rm "..repo_name_tag)
+                local ret2 = os.execute("docker run "..repo_name_tag)
+            end
+        }
+    }
 }
 EOF
 # 启动容器 -p openresty 指定启动标识-docker-compose的标识
